deba49a7c11b31a5f124743fa81f0e61833ec248
Alexifix <alexmsk77@bk.ru>
2023-02-15 09:42:07 +0300

    Add files via upload

---
+++ Calc/src/Calculator/Calculator.java
@@ -0,0 +1,8 @@
+package Calculator;
+
+import static Calculator.Calculation.StartCalculation;
+public class Calculator {
+    public static void main(String[] args) {
+        StartCalculation();
+    }
+}
